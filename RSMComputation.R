acab46cf29b5c2176b8be71d70b2b292cf5b47a3
Luke Donnet <Luke Donnet>
2017-09-02 02:16:36 +1000

    created first and second order model

--- RSMComputation.R
+++ RSMComputation.R
@@ -1,21 +1,34 @@
 # Import the RSM library
 library("rsm")
 
-#Create dummy data in a frame
-mockRsmCrudeYieldData.data <- 
+#Data for the RSM (Currently with dummy data)
+mockRsmCrudeYieldData.data <-
   data.frame(
     TempInCelsius = c(40, 40, 40, 40, 65, 65, 65, 65, 90, 90, 90, 90, 65, 65, 65),
-    pH = c(1, 2, 2, 3, 1, 3, 1, 3, 1, 2, 3, 2, 2, 2, 2), 
+    pH = c(1, 2, 2, 3, 1, 3, 1, 3, 1, 2, 3, 2, 2, 2, 2),
     TimeInMin = c(50, 10, 90, 50, 10, 90, 90, 10, 50, 10, 50, 90, 50, 50, 50),
     crudeYield = c(7, 2, 5, 2, 7, 3, 10, 2, 9, 6, 4, 8, 5, 5, 5)
   )
 
-png(file = "testPlot.png")
 
-# Extract vectors from the data frame
-temps = mockRsmCrudeYieldData.data$TempInCelsius
-yield = mockRsmCrudeYieldData.data$crudeYield
+# Create the code values for each parameter
+codedValues.val <- coded.data(mockRsmCrudeYieldData.data, 
+           x1 ~ (TempInCelsius - 65)/25,
+           x2 ~ (pH - 3),
+           x3 ~ (TimeInMin - 50)/40)
 
+# Define BBD function parameters
+numFactors = 4
+numCenterPts = 3
 
-plot(x = temps, y = yield)
+codedValues.rsm  <- rsm(crudeYield ~ SO(x1, x2, x3), data = codedValues.val)
+print(summary(codedValues.rsm))
+png(file = "testPlot3.png")
+
+#plot(codedValues.val)
+persp(codedValues.rsm,
+      ~ x1 + x2 + x3,
+      col = rainbow(50),
+      at = summary(codedValues.rsm)$canonical$xs
+)
 dev.off()
